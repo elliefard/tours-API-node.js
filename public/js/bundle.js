a5e307b100270bb597fffa9c20b14b7267194364
ellie fard <enesarifard@gmail.com>
2020-06-08 16:50:33 -0400

    V-2

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -8298,7 +8298,7 @@ function () {
             _context.next = 3;
             return (0, _axios.default)({
               method: 'POST',
-              url: 'http://127.0.0.1:3000/api/v1/users/signup',
+              url: '/api/v1/users/signup',
               data: {
                 name: name,
                 email: email,
